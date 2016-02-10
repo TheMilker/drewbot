4ab486cd4af29bb939cb1eaa14e8487d87359edf
Erik May <may.erik@gmail.com>
2016-02-09 23:00:23 -0800

    moving font creation stuff into its own component

---
+++ frontend/src/fontCreationControls/fontCreationControls.controller.js
@@ -0,0 +1,57 @@
+(function() {
+    'use strict'; 
+    angular.module('em-drewbot').controller('FontCreationControlsController', FontCreationControlsController);
+    
+    FontCreationControlsController.$inject = ['$http', 'drewbotService', 'simulatorDataService', 'strokeService'];
+    
+    function FontCreationControlsController($http, drewbotService, simulatorDataService, strokeService) {
+        var fontCreationControlsVM = this;
+        
+        fontCreationControlsVM.response = "";        
+        fontCreationControlsVM.simulatorModel = simulatorDataService.getSimulatorModel(); //TODO break off font creation data into its own model/service
+        fontCreationControlsVM.clearStrokes = simulatorDataService.clearStrokes;
+        fontCreationControlsVM.clearFontStrokes = simulatorDataService.clearFontStrokes;
+        
+        fontCreationControlsVM.sendStrokes = () => {
+            var JSONStrokes = simulatorDataService.getStrokesAsJSONArray();
+            console.log("Recorded Strokes: ", JSONStrokes);
+            $http.post('/drawStrokes', {strokes: JSONStrokes}).success((data, status, headers, config) => {
+                fontCreationControlsVM.response = data;
+            }).error((data, status, headers, config) => {
+                fontCreationControlsVM.response = data;
+            });
+        };
+        
+        fontCreationControlsVM.playStrokes = () => {
+            drewbotService.playStrokes();
+        };
+        
+        fontCreationControlsVM.makeFont = () => {
+            var JSONStrokes = simulatorDataService.getStrokesAsJSONArray();
+            JSONStrokes = strokeService.removeDuplicateStrokes(JSONStrokes);
+            JSONStrokes = strokeService.removeExtraUpStokes(JSONStrokes);
+            fontCreationControlsVM.simulatorModel.fontStrokes = JSON.stringify(JSONStrokes);
+        };
+        
+        fontCreationControlsVM.sendFont = () => {
+            var JSONStrokes = JSON.parse(fontCreationControlsVM.simulatorModel.fontStrokes);
+            console.log("Font Strokes: ", JSONStrokes);
+            $http.post('/drawStrokes', {strokes: JSONStrokes}).success((data, status, headers, config) => {
+                fontCreationControlsVM.response = data;
+            }).error((data, status, headers, config) => {
+                fontCreationControlsVM.response = data;
+            });
+        };
+        
+        fontCreationControlsVM.playFontStrokes = () => {
+            //drewbotService.playStrokes(); TODO maybe playStrokes() takes the strokes as a parameter
+        };
+
+        fontCreationControlsVM.recordingClicked = () => {
+            if(fontCreationControlsVM.simulatorModel.isRecording) {
+                simulatorDataService.clearStrokesAndCommands();
+                drewbotService.clearStrokePoints();
+            }
+        };
+    }
+})();
--- frontend/src/simulator/simulator.controller.js
+++ frontend/src/simulator/simulator.controller.js
@@ -9,58 +9,8 @@
         
         simulatorVM.response = "";        
         simulatorVM.simulatorModel = simulatorDataService.getSimulatorModel();
-        simulatorVM.clearCommands = simulatorDataService.clearCommands;
-        simulatorVM.clearStrokes = simulatorDataService.clearStrokes;
-        simulatorVM.clearFontStrokes = simulatorDataService.clearFontStrokes;
+        simulatorVM.clearCommands = simulatorDataService.clearCommands; //TODO Move command/message stuff to be part of the testControls, rename testControls to arduinoTestControls
         
-        simulatorVM.sendCommands = () => {
-            var commandsArray = simulatorDataService.getCommandsAsArray();
-            console.log("sending commands: ", commandsArray);
-            $http.post('/commands', {commands: commandsArray}).success((data, status, headers, config) => {
-                simulatorVM.response = data;
-            }).error((data, status, headers, config) => {
-                simulatorVM.response = data;
-            });
-        };
-
-        simulatorVM.sendStrokes = () => {
-            var JSONStrokes = simulatorDataService.getStrokesAsJSONArray();
-            console.log("Recorded Strokes: ", JSONStrokes);
-            $http.post('/drawStrokes', {strokes: JSONStrokes}).success((data, status, headers, config) => {
-                simulatorVM.response = data;
-            }).error((data, status, headers, config) => {
-                simulatorVM.response = data;
-            });
-        };
-        
-        simulatorVM.makeFont = () => {
-            var JSONStrokes = simulatorDataService.getStrokesAsJSONArray();
-            JSONStrokes = strokeService.removeDuplicateStrokes(JSONStrokes);
-            JSONStrokes = strokeService.removeExtraUpStokes(JSONStrokes);
-            simulatorVM.simulatorModel.fontStrokes = JSON.stringify(JSONStrokes);
-        };
-        
-        simulatorVM.sendFont = () => {
-            var JSONStrokes = JSON.parse(simulatorVM.simulatorModel.fontStrokes);
-            console.log("Font Strokes: ", JSONStrokes);
-            $http.post('/drawStrokes', {strokes: JSONStrokes}).success((data, status, headers, config) => {
-                simulatorVM.response = data;
-            }).error((data, status, headers, config) => {
-                simulatorVM.response = data;
-            });
-        };
-
-        simulatorVM.recordingClicked = () => {
-            if(simulatorVM.simulatorModel.isRecording) {
-                simulatorDataService.clearStrokesAndCommands();
-                drewbotService.clearStrokePoints();
-            }
-        };
-
-        simulatorVM.playStrokes = () => {
-            drewbotService.playStrokes();
-        };
-
         simulatorVM.messageKeypress = (event) => {
            if (event.keyCode == 13) {
               simulatorVM.doMessage();
