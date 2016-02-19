2aa1078438f91a548fa94abe600daeadb50fe8a7
Erik May <erik.may@emberex.com>
2016-02-18 22:56:48 -0800

    in progress moving to johnny-five from serial-port

--- backend/services/serialportService.js
+++ backend/services/serialportService.js
@@ -1,26 +1,49 @@
 var serialPort = require('serialport');
+var five = require("johnny-five");
+var repl = true;
+// var repl = false;
+var board = new five.Board({ repl: repl });
+
+var lifterServo;
+var leftServo;
+var rightServo;
+
+board.on("ready", () => {
+    lifterServo = new five.Servo(9);
+    leftServo = new five.Servo(10);
+    rightServo = new five.Servo(11);
+    // board.pinMode(9, five.Pin.SERVO);
+    // board.pinMode(10, five.Pin.SERVO);
+    // board.pinMode(11, five.Pin.SERVO);
+
+    board.repl.inject({
+        i: lifterServo,
+        l: leftServo,
+        r: rightServo
+    });
+});
 
 var port;
 
-connect();
+// connect();
 
-function connect() {
-    serialPort.list(function(err, ports) {
-        if(ports) {
-            console.log("Opening serial port: ", ports.slice(-1)[0].comName);
-            port = new serialPort.SerialPort(ports.slice(-1)[0].comName, null, false);
-            port.open(function(err) {
-                if(err) {
-                    console.log("on open err: ", err);
-                } else {
-                    console.log("serial port opened successfully!");
-                }
-            });
-        } else {
-            console.log("No devices found on serial ports.");
-        }
-    });
-}
+// function connect() {
+//     serialPort.list(function(err, ports) {
+//         if(ports) {
+//             console.log("Opening serial port: ", ports.slice(-1)[0].comName);
+//             port = new serialPort.SerialPort(ports.slice(-1)[0].comName, null, false);
+//             port.open(function(err) {
+//                 if(err) {
+//                     console.log("on open err: ", err);
+//                 } else {
+//                     console.log("serial port opened successfully!");
+//                 }
+//             });
+//         } else {
+//             console.log("No devices found on serial ports.");
+//         }
+//     });
+// }
 
 function writeCommand(command) {
     port.write(command, function(err, results) {
@@ -60,5 +83,5 @@ function writeCommands(commands) {
 module.exports = {
     writeCommand: writeCommand,
     writeCommands: writeCommands,
-    connect: connect
+    //connect: connect
 };
